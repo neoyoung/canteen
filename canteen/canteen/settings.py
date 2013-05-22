9a7d4d503e8367a4f1b309220ef5a907b8f21375
zhkzyth <zhkzyth@gmail.com>
2013-05-23 00:41:43 +0800

    fix static file bug

--- canteen/canteen/settings.py
+++ canteen/canteen/settings.py
@@ -77,7 +77,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-#STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'sitestatic')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -88,7 +88,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/zhkzyth/codeRep/canteen/canteen/static/',
+    os.path.join(PROJECT_DIR,'static'),
 )
 
 # List of finder classes that know how to find static files in
